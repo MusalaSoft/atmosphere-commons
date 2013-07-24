8a5a19e7b268bbb8481757aa56a72682c2fb0c7b
valyo.yolovski <valyo.yolovski@34708651-a5da-c34d-b20c-10ddab38317d>
2013-07-24 11:03:04 +0000

    Forgot to commit in previous submit.
    git-svn-id: https://trac.musala.com/ARDC/repo@8497 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/BatteryState.java
+++ src/com/musala/atmosphere/commons/BatteryState.java
@@ -9,19 +9,41 @@ package com.musala.atmosphere.commons;
 
 public enum BatteryState
 {
-	UNKNOWN("unknown"), CHARGING("charging"), DISCHARGING("discharging"), NOT_CHARGING("not_charging"), FULL("full");
+	UNKNOWN("unknown", 1), CHARGING("charging", 2), DISCHARGING("discharging", 3), NOT_CHARGING("not_charging", 4), FULL(
+			"full", 5);
 
-	private String value;
+	private String batteryStateValue;
 
-	private BatteryState(String deviceBatteryState)
+	private int batteryStateNumberValue;
+
+	private BatteryState(String deviceBatteryState, int stateNumber)
 	{
-		this.value = deviceBatteryState;
+		this.batteryStateValue = deviceBatteryState;
+		this.batteryStateNumberValue = stateNumber;
 	}
 
 	@Override
 	public String toString()
 	{
-		return value;
+		return batteryStateValue;
 	}
 
+	public int getStateId()
+	{
+		return batteryStateNumberValue;
+	}
+
+	public static BatteryState getStateById(int stateId)
+	{
+		BatteryState stateToReturn = null;
+		for (BatteryState state : BatteryState.values())
+		{
+			if (state.getStateId() == stateId)
+			{
+				stateToReturn = state;
+				break;
+			}
+		}
+		return stateToReturn;
+	}
 }
