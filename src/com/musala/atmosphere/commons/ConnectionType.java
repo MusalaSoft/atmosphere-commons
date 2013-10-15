8701107200ddd61faec8c00c0c1b38f8f02f16c6
yordan.petrov <yordan.petrov@34708651-a5da-c34d-b20c-10ddab38317d>
2013-10-15 09:08:06 +0000

    Initial import :  * ServiceRequest class used for sending requests to the ATMOSPHERE service. This class contains the previously implemented ServiceRequestProtocol class and some additional arguments;  * ConnectivityIntentListener added to the OnDeviceValdiator. This class listens for changes to the connectivity state;  * ConnectionTypeTest integration test.
    Changeset :
     * Requests sent to the service now contain additional arguments data;
     * AbstractWrapDevice.getConnectionType() now uses the ATMOSPHERE service;
     * OnDeviceValidator now presents info about the connection type of the device;
     * ConnectionType enumeration heavily refactored to mirror the constants presented by android.net.ConnectivityManager.
    
    [http://jenkins.musala.com/r/5914/ $5914] see #2570
    
    git-svn-id: https://trac.musala.com/ARDC/repo@8810 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/ConnectionType.java
+++ src/com/musala/atmosphere/commons/ConnectionType.java
@@ -1,29 +1,116 @@
 package com.musala.atmosphere.commons;
 
+/**
+ * Containing the different connection types as defined by android.net.ConnectivityManager.
+ * 
+ * @author yordan.petrov
+ * 
+ */
 public enum ConnectionType
 {
-	MOBILE("MOBILE"), WIFI("WIFI"), NONE("NONE");
+	/**
+	 * The Bluetooth data connection. When active, all data traffic will use this network type's interface by default
+	 * (it has a default route).
+	 */
+	BLUETOOTH(7),
 
-	String connectionType;
+	/**
+	 * Dummy data connection. This should not be used on shipping devices.
+	 */
+	DUMMY(8),
 
-	private ConnectionType(String connectionType)
+	/**
+	 * The Ethernet data connection. When active, all data traffic will use this network type's interface by default (it
+	 * has a default route).
+	 */
+	ETHERNET(9),
+
+	/**
+	 * The Mobile data connection. When active, all data traffic will use this network type's interface by default (it
+	 * has a default route).
+	 */
+	MOBILE(0),
+
+	/**
+	 * A DUN-specific Mobile data connection. This network type may use the same network interface as
+	 * {@link ConnectionType#MOBILE} or it may use a different one. This is sometimes by the system when setting up an
+	 * upstream connection for tethering so that the carrier is aware of DUN traffic.
+	 */
+	MOBILE_DUN(4),
+
+	/**
+	 * A High Priority Mobile data connection. This network type uses the same network interface as
+	 * {@link ConnectionType#MOBILE} but the routing setup is different.
+	 */
+	MOBILE_HIPRI(5),
+
+	/**
+	 * An MMS-specific Mobile data connection. This network type may use the same network interface as
+	 * {@link ConnectionType#MOBILE} or it may use a different one. This is used by applications needing to talk to the
+	 * carrier's Multimedia Messaging Service servers.
+	 */
+	MOBILE_MMS(2),
+
+	/**
+	 * A SUPL-specific Mobile data connection. This network type may use the same network interface as
+	 * {@link ConnectionType#MOBILE} or it may use a different one. This is used by applications needing to talk to the
+	 * carrier's Secure User Plane Location servers for help locating the device.
+	 */
+	MOBILE_SUPL(3),
+
+	/**
+	 * The WIFI data connection. When active, all data traffic will use this network type's interface by default (it has
+	 * a default route).
+	 */
+	WIFI(1),
+
+	/**
+	 * The WiMAX data connection. When active, all data traffic will use this network type's interface by default (it
+	 * has a default route).
+	 */
+	WIMAX(6),
+
+	/**
+	 * The device is not connected to any network.
+	 */
+	NONE(-1);
+
+	private int intValue;
+
+	private ConnectionType(int intValue)
 	{
-		this.connectionType = connectionType;
+		this.intValue = intValue;
 	}
 
-	ConnectionType getMemberByString(String typeString)
+	/**
+	 * Gets the connection type identifier.
+	 * 
+	 * @return the connection type identifier.
+	 */
+	public int getId()
 	{
-		ConnectionType typeToReturn = null;
-		for (ConnectionType connectionType : ConnectionType.values())
-		{
-			String connectionTypeString = connectionType.toString();
+		return intValue;
+	}
 
-			if (connectionTypeString.equals(typeString.toUpperCase()))
+	/**
+	 * Returns a {@link ConnectionType} object by a given identifier.
+	 * 
+	 * @param id
+	 *        - the connection type identifier.
+	 * @return a {@link ConnectionType} object by a given identifier.
+	 */
+	public static ConnectionType getById(int id)
+	{
+		ConnectionType typeById = null;
+		for (ConnectionType currentType : ConnectionType.values())
+		{
+			if (currentType.getId() == id)
 			{
-				typeToReturn = connectionType;
+				typeById = currentType;
 				break;
 			}
 		}
-		return typeToReturn;
+
+		return typeById;
 	}
 }
