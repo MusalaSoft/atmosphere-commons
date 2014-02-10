4d44854611ddf5eaf6f3409da3621d19fcfbc8bd
georgi.gaydarov <georgi.gaydarov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-02-10 09:47:40 +0000

    Changeset: * Refactored some class names (GesturePlayer -> UIAutomatorBridge); * Implemented logic behind gesture Client->Device transfer; * Moved several classes to their corresponding packages; * Fixed some project setup files; * Exception names refactoring; * Fixed a bug in the uiautomator background execution.
    [http://jenkins.musala.com/r/6740/ $6740] see #2754
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9648 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ src/com/musala/atmosphere/commons/gesture/Gesture.java
@@ -0,0 +1,19 @@
+package com.musala.atmosphere.commons.gesture;
+
+import java.util.LinkedList;
+
+/**
+ * Class that represents a gesture, consisting of one or several {@link Timeline} instances.
+ * 
+ * @author georgi.gaydarov
+ * 
+ */
+public class Gesture extends LinkedList<Timeline>
+{
+
+	/**
+	 * auto-generated serialization id
+	 */
+	private static final long serialVersionUID = -5546428458678128751L;
+
+}
