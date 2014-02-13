ee802fed7e654454e1c69fd47ee4a3bde060545c
georgi.gaydarov <georgi.gaydarov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-02-13 14:06:36 +0000

    Changeset: * Modified nearly all logic behind battery level, status and power connectivity setting and getting; * Fixed all unit tests related to the new changes; * Fixed all integration tests related to the new changes;
    [http://jenkins.musala.com/r/6774/ $6774] see #2745
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9663 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/beans/BatteryLevel.java
+++ src/com/musala/atmosphere/commons/beans/BatteryLevel.java
@@ -1,30 +1,34 @@
 package com.musala.atmosphere.commons.beans;
 
-import com.musala.atmosphere.commons.exceptions.CommandFailedException;
 import com.musala.atmosphere.commons.parameters.CommandParameter;
 
 /**
  * A class that represents the battery level command parameter.
- *
+ * 
  * @author boris.strandjev
  */
 public class BatteryLevel implements CommandParameter
 {
 	private int level;
 
-	public BatteryLevel(int level) throws CommandFailedException
+	/**
+	 * Constructs a bean that represents a battery capacity level.
+	 * 
+	 * @param level
+	 *        - the desired new battery level. This parameter should be between 0 and 100 inclusive, all other values
+	 *        will result in a {@link IllegalArgumentException} being thrown.
+	 */
+	public BatteryLevel(int level)
 	{
 		verifyLevel();
 		this.level = level;
 	}
 
-	private void verifyLevel() throws CommandFailedException
+	private void verifyLevel()
 	{
 		if (level < 0 || level > 100)
 		{
-			IllegalArgumentException illegalArgumentException = new IllegalArgumentException("Battery level should be in the range [0, 100].");
-			throw new CommandFailedException("Illegal argument has been passed to the emulator console class. "
-					+ "See the enclosed exception for more information.", illegalArgumentException);
+			throw new IllegalArgumentException("Battery level should be in the range [0, 100].");
 		}
 	}
 
@@ -34,4 +38,8 @@ public class BatteryLevel implements CommandParameter
 		return String.valueOf(level);
 	}
 
+	public int getLevel()
+	{
+		return level;
+	}
 }
---
+++ src/com/musala/atmosphere/commons/beans/PowerSource.java
@@ -0,0 +1,71 @@
+package com.musala.atmosphere.commons.beans;
+
+import java.io.Serializable;
+
+import com.musala.atmosphere.commons.parameters.CommandParameter;
+
+/**
+ * Enumerator for device power supply source. Values used are as seen on <a
+ * href="http://developer.android.com/reference/android/os/BatteryManager.html">Android Developer BatteryManager
+ * documentation.</a>
+ * 
+ * @author georgi.gaydarov
+ * 
+ */
+public enum PowerSource implements CommandParameter, Serializable
+{
+	/**
+	 * Device is unplugged.
+	 */
+	UNPLUGGED(0),
+	/**
+	 * Power source is an AC charger.
+	 */
+	PLUGGED_AC(1),
+	/**
+	 * Power source is a USB port.<br>
+	 * <b>Note</b> : For emulators, this option is interpreted as {@link #PLUGGED_AC}.
+	 */
+	PLUGGED_USB(2),
+	/**
+	 * Power source is wireless.<br>
+	 * <b>Note</b> : For emulators, this option is interpreted as {@link #PLUGGED_AC}.
+	 */
+	PLUGGED_WIRELESS(4);
+
+	private int stateId;
+
+	private PowerSource(int stateId)
+	{
+		this.stateId = stateId;
+	}
+
+	public int getStateId()
+	{
+		return stateId;
+	}
+
+	public static PowerSource getStateById(int stateId)
+	{
+		PowerSource stateToReturn = null;
+		for (PowerSource state : PowerSource.values())
+		{
+			if (state.getStateId() == stateId)
+			{
+				stateToReturn = state;
+				break;
+			}
+		}
+		return stateToReturn;
+	}
+
+	@Override
+	public String getParameterValue(boolean forEmulator)
+	{
+		if (forEmulator)
+		{
+			return (this == UNPLUGGED ? "off" : "on");
+		}
+		return toString();
+	}
+}
