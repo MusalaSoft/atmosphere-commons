1522cb646a8614259b853461bf3e331d37a61870
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-03-05 16:36:18 +0000

    Changeset : * Fix setLocked method and now it works on all Samsung devices.
    [http://jenkins.musala.com/r/7058/ $7058] see #2855
    
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9725 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/DeviceInformation.java
+++ src/com/musala/atmosphere/commons/DeviceInformation.java
@@ -12,283 +12,288 @@ import com.musala.atmosphere.commons.util.Pair;
  * @author georgi.gaydarov
  * 
  */
-public class DeviceInformation implements Serializable
-{
-	public static final Integer FALLBACK_DISPLAY_DENSITY = 0;
+public class DeviceInformation implements Serializable {
+    public static final Integer FALLBACK_DISPLAY_DENSITY = 0;
 
-	public static final Integer FALLBACK_RAM_AMOUNT = 0;
+    public static final Integer FALLBACK_RAM_AMOUNT = 0;
 
-	public static final Pair<Integer, Integer> FALLBACK_SCREEN_RESOLUTION = new Pair<Integer, Integer>(0, 0);
+    public static final Pair<Integer, Integer> FALLBACK_SCREEN_RESOLUTION = new Pair<Integer, Integer>(0, 0);
 
-	public static final boolean FALLBACK_ISEMULATOR_SETTING = true;
+    public static final boolean FALLBACK_ISEMULATOR_SETTING = true;
 
-	public static final String FALLBACK_MODEL_VALUE = "not set";
+    public static final String FALLBACK_MODEL_VALUE = "not set";
 
-	public static final String FALLBACK_OS_VALUE = "not set";
+    public static final String FALLBACK_OS_VALUE = "not set";
 
-	public static final String FALLBACK_SERIALNUMBER_VALUE = "not set";
+    public static final String FALLBACK_SERIALNUMBER_VALUE = "not set";
 
-	public static final String FALLBACK_CPU_IDENTIFIER = "unknown";
+    public static final String FALLBACK_CPU_IDENTIFIER = "unknown";
 
-	public static final int FALLBACK_API_LEVEL = 0;
+    public static final String FALLBACK_MANUFACTURER_NAME = "not set";
 
-	/**
-	 * Default constructor that fills this structure with the FALLBACK constants.
-	 */
-	public DeviceInformation()
-	{
-		isEmulator = FALLBACK_ISEMULATOR_SETTING;
-		dpi = FALLBACK_DISPLAY_DENSITY;
-		model = FALLBACK_MODEL_VALUE;
-		os = FALLBACK_OS_VALUE;
-		resolution = FALLBACK_SCREEN_RESOLUTION;
-		serialNumber = FALLBACK_SERIALNUMBER_VALUE;
-		ram = FALLBACK_RAM_AMOUNT;
-		cpu = FALLBACK_CPU_IDENTIFIER;
-		apiLevel = FALLBACK_API_LEVEL;
-	}
+    public static final int FALLBACK_API_LEVEL = 0;
 
-	/**
+    /**
+     * Default constructor that fills this structure with the FALLBACK constants.
+     */
+    public DeviceInformation() {
+        isEmulator = FALLBACK_ISEMULATOR_SETTING;
+        dpi = FALLBACK_DISPLAY_DENSITY;
+        model = FALLBACK_MODEL_VALUE;
+        os = FALLBACK_OS_VALUE;
+        resolution = FALLBACK_SCREEN_RESOLUTION;
+        serialNumber = FALLBACK_SERIALNUMBER_VALUE;
+        ram = FALLBACK_RAM_AMOUNT;
+        cpu = FALLBACK_CPU_IDENTIFIER;
+        apiLevel = FALLBACK_API_LEVEL;
+        manufacturer = FALLBACK_MANUFACTURER_NAME;
+    }
+
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = -6289200803789024053L;
+    private static final long serialVersionUID = -6289200803789024053L;
 
-	/**
-	 * Serial number of the device.
-	 */
-	private String serialNumber;
+    /**
+     * Serial number of the device.
+     */
+    private String serialNumber;
 
-	/**
-	 * True if this device is an emulator, false if the device is physical.
-	 */
-	private boolean isEmulator;
+    /**
+     * True if this device is an emulator, false if the device is physical.
+     */
+    private boolean isEmulator;
 
-	/**
-	 * Device screen resolution.
-	 */
-	private Pair<Integer, Integer> resolution;
+    /**
+     * Device screen resolution.
+     */
+    private Pair<Integer, Integer> resolution;
 
-	/**
-	 * Operating system of the device.
-	 */
-	private String os;
+    /**
+     * Operating system of the device.
+     */
+    private String os;
 
-	/**
-	 * Device model.
-	 */
-	private String model;
+    /**
+     * Device model.
+     */
+    private String model;
 
-	/**
-	 * Dots per inch on the device screen.
-	 */
-	private int dpi;
+    /**
+     * Dots per inch on the device screen.
+     */
+    private int dpi;
 
-	/**
-	 * Random access memory (RAM) available to the device.
-	 */
-	private int ram;
+    /**
+     * Random access memory (RAM) available to the device.
+     */
+    private int ram;
 
-	/**
-	 * CPU identifier.
-	 */
-	private String cpu;
+    /**
+     * CPU identifier.
+     */
+    private String cpu;
 
-	/**
-	 * Device API level.
-	 */
-	private int apiLevel;
+    /**
+     * Device API level.
+     */
+    private int apiLevel;
 
-	/**
-	 * @return the serial number of the device.
-	 */
-	public String getSerialNumber()
-	{
-		return serialNumber;
-	}
+    /**
+     * Device manufacturer
+     */
+    private String manufacturer;
 
-	/**
-	 * Sets device serial number in this DeviceInformation.
-	 * 
-	 * @param serialNumber
-	 *        - the serial number to be set.
-	 */
-	public void setSerialNumber(String serialNumber)
-	{
-		this.serialNumber = serialNumber;
-	}
+    /**
+     * @return the serial number of the device.
+     */
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 
-	/**
-	 * @return <code>true</code> if this device is an emulator.<br>
-	 *         <code>false</code> if the device is real physical device.
-	 */
-	public boolean isEmulator()
-	{
-		return isEmulator;
-	}
+    /**
+     * Sets device serial number in this DeviceInformation.
+     * 
+     * @param serialNumber
+     *        - the serial number to be set.
+     */
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 
-	/**
-	 * Sets device type in this DeviceInformation.
-	 * 
-	 * @param isEmulator
-	 *        - <code>true</code> for emulator, <code>false</code> for real device.
-	 */
-	public void setEmulator(boolean isEmulator)
-	{
-		this.isEmulator = isEmulator;
-	}
+    /**
+     * @return <code>true</code> if this device is an emulator.<br>
+     *         <code>false</code> if the device is real physical device.
+     */
+    public boolean isEmulator() {
+        return isEmulator;
+    }
 
-	/**
-	 * 
-	 * @return {@link Pair} object, containing the resolution (width, height) of device's screen.
-	 */
-	public Pair<Integer, Integer> getResolution()
-	{
-		return resolution;
-	}
+    /**
+     * Sets device type in this DeviceInformation.
+     * 
+     * @param isEmulator
+     *        - <code>true</code> for emulator, <code>false</code> for real device.
+     */
+    public void setEmulator(boolean isEmulator) {
+        this.isEmulator = isEmulator;
+    }
 
-	/**
-	 * Sets the device resolution in this DeviceInformation.
-	 * 
-	 * @param resolution
-	 *        - (width, height) pair to be set as device's resolution.
-	 */
-	public void setResolution(Pair<Integer, Integer> resolution)
-	{
-		this.resolution = resolution;
-	}
+    /**
+     * 
+     * @return {@link Pair} object, containing the resolution (width, height) of device's screen.
+     */
+    public Pair<Integer, Integer> getResolution() {
+        return resolution;
+    }
 
-	/**
-	 * 
-	 * @return device's operating system.
-	 */
-	public String getOS()
-	{
-		return os;
-	}
+    /**
+     * Sets the device resolution in this DeviceInformation.
+     * 
+     * @param resolution
+     *        - (width, height) pair to be set as device's resolution.
+     */
+    public void setResolution(Pair<Integer, Integer> resolution) {
+        this.resolution = resolution;
+    }
 
-	/**
-	 * Sets the device's operating system in this DeviceInformation.
-	 * 
-	 * @param os
-	 *        - operating system to set.
-	 */
-	public void setOs(String os)
-	{
-		this.os = os;
-	}
+    /**
+     * 
+     * @return device's operating system.
+     */
+    public String getOS() {
+        return os;
+    }
 
-	/**
-	 * 
-	 * @return Android API level of device.
-	 */
-	public int getApiLevel()
-	{
-		return apiLevel;
-	}
+    /**
+     * Sets the device's operating system in this DeviceInformation.
+     * 
+     * @param os
+     *        - operating system to set.
+     */
+    public void setOs(String os) {
+        this.os = os;
+    }
 
-	/**
-	 * Sets device's Android API level in this DeviceInformation.
-	 * 
-	 * @param os
-	 *        - operating system to set.
-	 */
-	public void setApiLevel(int apiLevel)
-	{
-		this.apiLevel = apiLevel;
-	}
+    /**
+     * 
+     * @return Android API level of device.
+     */
+    public int getApiLevel() {
+        return apiLevel;
+    }
 
-	/**
-	 * 
-	 * @return device model.
-	 */
-	public String getModel()
-	{
-		return model;
-	}
+    /**
+     * Sets device's Android API level in this DeviceInformation.
+     * 
+     * @param os
+     *        - operating system to set.
+     */
+    public void setApiLevel(int apiLevel) {
+        this.apiLevel = apiLevel;
+    }
 
-	/**
-	 * Sets device model in this DeviceInformation.
-	 * 
-	 * @param model
-	 *        - device model to set.
-	 */
-	public void setModel(String model)
-	{
-		this.model = model;
-	}
+    /**
+     * 
+     * @return device model.
+     */
+    public String getModel() {
+        return model;
+    }
 
-	/**
-	 * 
-	 * @return the dpi (dots per inch) of device's screen.
-	 */
-	public int getDpi()
-	{
-		return dpi;
-	}
+    /**
+     * Sets device model in this DeviceInformation.
+     * 
+     * @param model
+     *        - device model to set.
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
 
-	/**
-	 * Sets device screen dpi (dots per inch) in this DeviceInformation.
-	 * 
-	 * @param dpi
-	 *        - screen dpi to set.
-	 */
-	public void setDpi(int dpi)
-	{
-		this.dpi = dpi;
-	}
+    /**
+     * 
+     * @return the dpi (dots per inch) of device's screen.
+     */
+    public int getDpi() {
+        return dpi;
+    }
 
-	/**
-	 * 
-	 * @return total device RAM.
-	 */
-	public int getRam()
-	{
-		return ram;
-	}
+    /**
+     * Sets device screen dpi (dots per inch) in this DeviceInformation.
+     * 
+     * @param dpi
+     *        - screen dpi to set.
+     */
+    public void setDpi(int dpi) {
+        this.dpi = dpi;
+    }
 
-	/**
-	 * Sets total device RAM in this DeviceInformation.
-	 * 
-	 * @param ram
-	 *        - device RAM to set.
-	 */
-	public void setRam(int ram)
-	{
-		this.ram = ram;
-	}
+    /**
+     * 
+     * @return total device RAM.
+     */
+    public int getRam() {
+        return ram;
+    }
 
-	/**
-	 * Sets device CPU model identifier in this DeviceInformation.
-	 * 
-	 * @param cpu
-	 *        - device CPU model to set.
-	 */
-	public void setCpu(String cpu)
-	{
-		this.cpu = cpu;
-	}
+    /**
+     * Sets total device RAM in this DeviceInformation.
+     * 
+     * @param ram
+     *        - device RAM to set.
+     */
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
 
-	/**
-	 * 
-	 * @return device CPU model identifier.
-	 */
-	public String getCpu()
-	{
-		return cpu;
-	}
+    /**
+     * Sets device CPU model identifier in this DeviceInformation.
+     * 
+     * @param cpu
+     *        - device CPU model to set.
+     */
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
 
-	@Override
-	public String toString()
-	{
-		return String.format(	"[Emulator: %s, Model: %s, OS: %s, API: %s, RAM: %s, CPU: %s, Resolution: %s, Dpi: %s, SerialNo: %s]",
-								isEmulator,
-								model,
-								os,
-								apiLevel,
-								ram,
-								cpu,
-								resolution,
-								dpi,
-								serialNumber);
-	}
+    /**
+     * 
+     * @return device CPU model identifier.
+     */
+    public String getCpu() {
+        return cpu;
+    }
+
+    /**
+     * Sets device manufacturer.
+     * 
+     * @parm manufacturer - device manufacturer to set.
+     */
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    /**
+     * 
+     * @return device manufacturer.
+     */
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Emulator: %s, Model: %s, OS: %s, API: %s, RAM: %s, CPU: %s, Resolution: %s, Dpi: %s, SerialNo: %s, Manufacturer: %s]",
+                             isEmulator,
+                             model,
+                             os,
+                             apiLevel,
+                             ram,
+                             cpu,
+                             resolution,
+                             dpi,
+                             serialNumber,
+                             manufacturer);
+    }
 }
