9fd675eccc45bc91f0e60b0cdf6f88fb3092b76e
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-05-21 12:40:12 +0000

    Changeset : * Add getter in Device class for getting screen orientation.
    [http://jenkins.musala.com/r/8287/ $8287] see #3154
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9993 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/ScreenOrientation.java
+++ src/com/musala/atmosphere/commons/ScreenOrientation.java
@@ -6,19 +6,28 @@ package com.musala.atmosphere.commons;
  * @author nikola.taushanov
  * 
  */
-public enum ScreenOrientation
-{
-	LANDSCAPE(1), PORTRAIT(0), UPSIDE_DOWN_LANDSCAPE(3), UPSIDE_DOWN_PORTRAIT(2);
+public enum ScreenOrientation {
+    LANDSCAPE(1),
+    PORTRAIT(0),
+    UPSIDE_DOWN_LANDSCAPE(3),
+    UPSIDE_DOWN_PORTRAIT(2);
 
-	private int orientationNumber;
+    private int orientationNumber;
 
-	private ScreenOrientation(int orientationNumber)
-	{
-		this.orientationNumber = orientationNumber;
-	}
+    private ScreenOrientation(int orientationNumber) {
+        this.orientationNumber = orientationNumber;
+    }
 
-	public int getOrientationNumber()
-	{
-		return orientationNumber;
-	}
+    public int getOrientationNumber() {
+        return orientationNumber;
+    }
+
+    public static ScreenOrientation getValueOfInt(int orientationNumber) {
+        for (ScreenOrientation currentOrientation : ScreenOrientation.values()) {
+            if (currentOrientation.getOrientationNumber() == orientationNumber) {
+                return currentOrientation;
+            }
+        }
+        return null;
+    }
 }
