20795f64ea66cce81c6634df417327be618ab5d3
filareta.yordanova <filareta.yordanova@musala.com>
2015-05-27 15:39:58 +0300

    Create Matcher Working with UiElementPropertiesContainer and use it in checking for UI element existence in the traverser, see #43

---
+++ src/com/musala/atmosphere/commons/ui/tree/matcher/UiElementPropertiesContainerMatcher.java
@@ -0,0 +1,70 @@
+package com.musala.atmosphere.commons.ui.tree.matcher;
+
+import android.graphics.Rect;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.musala.atmosphere.commons.geometry.Bounds;
+import com.musala.atmosphere.commons.geometry.Point;
+import com.musala.atmosphere.commons.ui.UiElementPropertiesContainer;
+
+/**
+ * Concrete {@link UiElementMatcher matcher} that determines whether a match between {@link AccessibilityNodeInfo
+ * accessibility node information} and element from type {@link UiElementPropertiesContainer
+ * UiElementPropertiesContainer} exists.
+ * 
+ * @author filareta.yordanova
+ *
+ */
+public class UiElementPropertiesContainerMatcher implements UiElementMatcher<UiElementPropertiesContainer> {
+
+    @Override
+    public boolean match(UiElementPropertiesContainer propertiesContainer, AccessibilityNodeInfo nodeInformation) {
+        Rect accessibilityNodeBounds = new Rect();
+        nodeInformation.getBoundsInScreen(accessibilityNodeBounds);
+        Bounds propertiesContainerBounds = propertiesContainer.getBounds();
+
+        if (propertiesContainer.getBounds() != null) {
+            Point selectorBoundsUpperCorner = propertiesContainerBounds.getUpperLeftCorner();
+
+            if ((accessibilityNodeBounds.height() != propertiesContainerBounds.getHeight())
+                    || (accessibilityNodeBounds.width() != propertiesContainerBounds.getWidth())
+                    || (selectorBoundsUpperCorner.getX() != accessibilityNodeBounds.left)
+                    || (selectorBoundsUpperCorner.getY() != accessibilityNodeBounds.top)) {
+                return false;
+            }
+        }
+
+        if (propertiesContainer.getClassName() != null && nodeInformation.getClassName() != null
+                && !propertiesContainer.getClassName().equals(nodeInformation.getClassName().toString())) {
+            return false;
+        }
+
+        if (propertiesContainer.getPackageName() != null && nodeInformation.getPackageName() != null
+                && !propertiesContainer.getPackageName().equals(nodeInformation.getPackageName().toString())) {
+            return false;
+        }
+
+        if (propertiesContainer.getContentDescriptor() != null
+                && nodeInformation.getContentDescription() != null
+                && !propertiesContainer.getContentDescriptor().equals(nodeInformation.getContentDescription()
+                                                                                     .toString())) {
+            return false;
+        }
+
+        if (propertiesContainer.getText() != null && nodeInformation.getText() != null
+                && !propertiesContainer.getText().equals(nodeInformation.getText().toString())) {
+            return false;
+        }
+
+        return propertiesContainer.isCheckable() == nodeInformation.isCheckable()
+                && propertiesContainer.isChecked() == nodeInformation.isChecked()
+                && propertiesContainer.isClickable() == nodeInformation.isClickable()
+                && propertiesContainer.isEnabled() == nodeInformation.isEnabled()
+                && propertiesContainer.isFocusable() == nodeInformation.isFocusable()
+                && propertiesContainer.isFocused() == nodeInformation.isFocused()
+                && propertiesContainer.isLongClickable() == nodeInformation.isLongClickable()
+                && propertiesContainer.isPassword() == nodeInformation.isPassword()
+                && propertiesContainer.isScrollable() == nodeInformation.isScrollable()
+                && propertiesContainer.isSelected() == nodeInformation.isSelected();
+    }
+}
