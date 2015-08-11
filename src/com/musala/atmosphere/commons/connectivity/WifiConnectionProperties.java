3fe2a138e42174a3cbcce4decd54fbb15d55564e
filareta.yordanova <filareta.yordanova@musala.com>
2015-08-11 15:26:21 +0300

    Set all unused wifi connection property fields to default value.
    see #96

--- src/com/musala/atmosphere/commons/connectivity/WifiConnectionProperties.java
+++ src/com/musala/atmosphere/commons/connectivity/WifiConnectionProperties.java
@@ -30,14 +30,15 @@ public class WifiConnectionProperties implements Serializable {
      * @return string in JSON format
      */
     public String serialize() {
+        // TODO: Use suitable library for JSON serialization, e.g. GSON
         return "{\"up\":{\"rate\":" + upLink.getRate() + ",\"delay\":{\"delay\":" + upLink.getLatency()
                 + ",\"jitter\":0,\"correlation\":0}," + "\"loss\":{\"percentage\":" + upLink.getLoss()
                 + ",\"correlation\":0}," + "\"reorder\":{\"percentage\":\"0\",\"correlation\":0,\"gap\":0},"
                 + "\"corruption\":{\"percentage\":\"0\",\"correlation\":0},\"iptables_options\":[]},"
                 + "\"down\":{\"rate\":" + downLink.getRate() + ",\"delay\":{\"delay\":" + downLink.getLatency()
                 + ",\"jitter\":0,\"correlation\":0}," + "\"loss\":{\"percentage\":" + downLink.getLoss()
-                + ",\"correlation\":0}," + "\"reorder\":{\"percentage\":\"5\",\"correlation\":0,\"gap\":0},"
-                + "\"corruption\":{\"percentage\":\"4\",\"correlation\":0},\"iptables_options\":[]}}";
+                + ",\"correlation\":0}," + "\"reorder\":{\"percentage\":\"0\",\"correlation\":0,\"gap\":0},"
+                + "\"corruption\":{\"percentage\":\"0\",\"correlation\":0},\"iptables_options\":[]}}";
 
     }
 }
