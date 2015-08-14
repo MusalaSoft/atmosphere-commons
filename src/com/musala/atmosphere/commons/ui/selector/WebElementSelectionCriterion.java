68d99371bf9a3369a272471a7123ea2944045c49
Denis Bialev <denis.bialev@musala.com>
2015-08-14 16:41:30 +0300

    Add class representing Element from a WebView containing most of the possible WebElement interactions. Add abstract class webElement containing common logic between UiWebElement and WebView. Add enum for selecting WebElements.
    see #105, see #109
    
    Change-Id: Iebacda24761823ff75115160fa232890413a51cb

---
+++ src/com/musala/atmosphere/commons/ui/selector/WebElementSelectionCriterion.java
@@ -0,0 +1,26 @@
+package com.musala.atmosphere.commons.ui.selector;
+
+/**
+ * Enum containing the attributes by which a {@link WebElement} can be selected.
+ * 
+ * @author denis.bialev
+ *
+ */
+public enum WebElementSelectionCriterion {
+    /** Value denoting the 'class' attribute. Type of needed value String. */
+    CLASS,
+    /** Value denoting the 'tag' attribute. Type of needed value String. */
+    TAG,
+    /** Value denoting the 'id' attribute. Type of needed value String. */
+    ID,
+    /** Value denoting the 'CssSelector' attribute. Type of needed value CssQuery in String. */
+    CSS_SELECTOR,
+    /** Value denoting the 'xpath' attribute. Type of needed value Xpath query in string. */
+    XPATH,
+    /** Value denoting the 'name' attribute. Type of needed value String. */
+    NAME,
+    /** Value denoting the 'link' attribute. Type of needed value String. */
+    LINK,
+    /** Value denoting the partial 'link' attribute. Type of needed value String. */
+    PARTIAL_LINK;
+}
