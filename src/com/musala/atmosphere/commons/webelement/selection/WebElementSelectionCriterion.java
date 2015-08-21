fbca321a88048624663df3da5d3af4ffe62f1f92
filareta.yordanova <filareta.yordanova@musala.com>
2015-08-21 19:14:12 +0300

    Store Proper Selection Criterion When Finding List of Web Elements
    see #123
    
    Change-Id: I0a8018763c8ffbf0e4fe1b36d82cc11098bb6a72
    Conflicts:
    	ARDC/Source/ATMOSPHERE/OnDeviceValidator/assets/web_view_sample.html

--- src/com/musala/atmosphere/commons/webelement/selection/WebElementSelectionCriterion.java
+++ src/com/musala/atmosphere/commons/webelement/selection/WebElementSelectionCriterion.java
@@ -8,19 +8,29 @@ package com.musala.atmosphere.commons.webelement.selection;
  */
 public enum WebElementSelectionCriterion {
     /** Value denoting the 'class' attribute. Type of needed value String. */
-    CLASS,
+    CLASS("class"),
     /** Value denoting the 'tag' attribute. Type of needed value String. */
-    TAG,
+    TAG("tag"),
     /** Value denoting the 'id' attribute. Type of needed value String. */
-    ID,
+    ID("id"),
     /** Value denoting the 'CssSelector' attribute. Type of needed value CssQuery in String. */
-    CSS_SELECTOR,
+    CSS_SELECTOR("css_selector"),
     /** Value denoting the 'xpath' attribute. Type of needed value Xpath query in string. */
-    XPATH,
+    XPATH("xpath"),
     /** Value denoting the 'name' attribute. Type of needed value String. */
-    NAME,
+    NAME("name"),
     /** Value denoting the 'link' attribute. Type of needed value String. */
-    LINK,
+    LINK("href"),
     /** Value denoting the partial 'link' attribute. Type of needed value String. */
-    PARTIAL_LINK;
+    PARTIAL_LINK("href");
+
+    private String name;
+
+    private WebElementSelectionCriterion(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
